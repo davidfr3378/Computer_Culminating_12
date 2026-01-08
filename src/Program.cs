faf20fe6bf68d51fafa427e216c1045cac72f755
davidfr3378 <157181930+davidfr3378@users.noreply.github.com>
2026-01-08 12:28:45 -0500

    Add main program structure in Program.cs

---
+++ src/Program.cs
@@ -0,0 +1,22 @@
+/*
+ * @author david.ez
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Console.WriteLine("*************************");
+        Console.WriteLine("Program startup complete");
+        //Code between
+        
+        Shell shell = new Shell();
+        shell.shell();
+        
+        //Code between
+        Console.WriteLine("Program Instance Closed");
+        Console.WriteLine("*************************");
+        
+    }
+    
+}
